a0e875549e3b2ed82f4e74101cbb4af86fc9c442
wladyslaw <vladislavvaliuchenkov@gmail.com>
2024-10-06 18:18:05 -0700

    removed projects_path util

--- apps/@shared/core/src/projects/utils.rs
+++ apps/@shared/core/src/projects/utils.rs
@@ -2,22 +2,11 @@ use crate::types::ProjectFull;
 use anyhow::{Context, Result};
 use serde_json::{from_slice, to_vec};
 use std::{
-  fs::{create_dir_all, read, write},
+  fs::{read, write},
   path::{Path, PathBuf},
 };
 
 pub const PROJECT_FILE_EXT: &str = ".shogproj";
-
-/// get path to projects directory
-/// * `app_dir` - path to app data directory. It may be different on client & server
-pub fn projects_path(app_dir: impl AsRef<Path>) -> PathBuf {
-  let path = app_dir.as_ref().join("projects");
-
-  // or create if it doesn't exist
-  create_dir_all(&path).expect("Failed to create projects directory");
-
-  path
-}
 
 /// get project file path
 /// * `dir` - path to projects directory. It's different on client & server
