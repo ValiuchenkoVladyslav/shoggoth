4926153acde8cd463fe32ae5b1c865ed66bfe85e
wladyslaw <vladislavvaliuchenkov@gmail.com>
2024-10-06 18:29:38 -0700

    added more utils

--- apps/client/src/utils.ts
+++ apps/client/src/utils.ts
@@ -1,6 +1,22 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+/** tailwind util to merge classes */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+/**
+ * create a new id
+ * @param prefix prefix for the id
+ */
+export function newId(prefix = "") {
+  return prefix + Math.random() + "" + Math.random();
+}
+
+/** zustand type helper for store creation */
+export type InferParams<T extends { setState: unknown; getState: unknown }> = [
+  T["setState"],
+  T["getState"],
+  T,
+];
