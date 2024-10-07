3ae4e637636e175b22140cc7c9876fdcea1e1c74
wladyslaw <vladislavvaliuchenkov@gmail.com>
2024-10-06 18:20:03 -0700

    added mod for common imports

---
+++ apps/client/src-tauri/src/common.rs
@@ -0,0 +1,8 @@
+pub use tauri::{AppHandle as App, Manager, Result as CmdRes};
+
+pub use crate::app_dirs::AppDirs;
+
+pub use serde::{Deserialize, Serialize};
+pub use ts_rs::TS;
+
+pub use anyhow::Error as AnyErr;
