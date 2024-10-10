cf9d624bc51aeb65a36d696ec6e581a6768207e9
wladyslaw <vladislavvaliuchenkov@gmail.com>
2024-10-10 05:23:11 -0700

    added default for schema

--- apps/@shared/core/src/utils.rs
+++ apps/@shared/core/src/utils.rs
@@ -5,7 +5,7 @@ macro_rules! schema {
       $($(#[$field_attr:meta])* $field:ident: $typ:ty $(,)?),*
     }
   ) => {
-    #[derive(ts_rs::TS, serde::Serialize, serde::Deserialize $(, $($derives),*)?)]
+    #[derive(ts_rs::TS, serde::Serialize, serde::Deserialize, Debug $(, $($derives),*)?)]
     #[ts(export, export_to = "./index.ts")]
     pub struct $name {
       $($(#[$field_attr])* pub $field: $typ),*
