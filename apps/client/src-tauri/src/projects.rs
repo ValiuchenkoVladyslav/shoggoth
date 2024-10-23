e12093cc328ec9ab14a2d63d3633d1c0a9723bf0
wladyslaw <vladislavvaliuchenkov@gmail.com>
2024-10-23 05:52:47 -0700

    changed projects module to single file

--- apps/client/src-tauri/src/projects.rs
+++ apps/client/src-tauri/src/projects.rs
@@ -1,36 +1,38 @@
-use shogg_core::{
-  projects,
-  types::{DataGraph, ProjectBase, ProjectMeta},
+//! project commands
+
+use shogg_core::projects::{
+  self,
+  schemas::{DataGraph, ProjectBase, ProjectMeta},
 };
 
 use crate::utils::*;
 
-#[tauri::command(rename_all = "snake_case")]
+#[command(rename_all = "snake_case")]
 pub fn get_projects(app: App) -> CmdRes<Vec<ProjectMeta>> {
   Ok(projects::read_projects(app.projects_dir())?)
 }
 
-#[tauri::command(rename_all = "snake_case")]
+#[command(rename_all = "snake_case")]
 pub fn edit_meta(app: App, meta: ProjectMeta) -> CmdRes {
   Ok(projects::write_meta(app.projects_dir(), meta)?)
 }
 
-#[tauri::command(rename_all = "snake_case")]
+#[command(rename_all = "snake_case")]
 pub fn edit_graph(app: App, id: &str, graph: DataGraph) -> CmdRes {
   Ok(projects::write_graph(app.projects_dir(), id, graph)?)
 }
 
-#[tauri::command(rename_all = "snake_case")]
+#[command(rename_all = "snake_case")]
 pub fn get_graph(app: App, id: &str) -> CmdRes<DataGraph> {
   Ok(projects::read_graph(app.projects_dir(), id)?)
 }
 
-#[tauri::command(rename_all = "snake_case")]
+#[command(rename_all = "snake_case")]
 pub fn create_project(app: App, project_base: ProjectBase) -> CmdRes<ProjectMeta> {
   Ok(projects::create_project(app.projects_dir(), project_base)?)
 }
 
-#[tauri::command(rename_all = "snake_case")]
+#[command(rename_all = "snake_case")]
 pub fn delete_project(app: App, id: &str) -> CmdRes {
   Ok(projects::delete_project(app.projects_dir(), id)?)
 }
