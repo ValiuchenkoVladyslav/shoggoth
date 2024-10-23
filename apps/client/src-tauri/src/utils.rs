ea529f5e3d3f3352acae6c6ccd40e70153bc5b77
wladyslaw <vladislavvaliuchenkov@gmail.com>
2024-10-23 05:59:53 -0700

    added app state utility type

--- apps/client/src-tauri/src/utils.rs
+++ apps/client/src-tauri/src/utils.rs
@@ -1,12 +1,14 @@
-use std::{ffi::OsStr, fs, io, path::PathBuf};
+//! common utilities for the app
+
+use std::{ffi::OsStr, fs, io, path::PathBuf, str};
 use tokio::process::Command;
 
 // re-export common imports
 pub use anyhow::Error as AnyErr;
 pub use shogg_core::{schema, CtxExt};
-pub use tauri::{AppHandle as App, Manager};
+pub use tauri::{command, AppHandle as App, Manager};
 
-/// utility to spawn a command (it won't open a cmd on windows)
+/// spawn a command (it won't open a cmd on windows)
 pub fn cmd(cmd: impl AsRef<OsStr>) -> Command {
   let mut command = Command::new(cmd);
 
@@ -19,12 +21,15 @@ pub fn cmd(cmd: impl AsRef<OsStr>) -> Command {
 /// tauri command return type
 pub type CmdRes<T = ()> = tauri::Result<T>;
 
+/// actix data type
+pub type AppState = actix_web::web::Data<App>;
+
 /// `Vec<u8>` to `String`
 pub fn bytes_string(bytes: Vec<u8>) -> String {
-  std::str::from_utf8(&bytes).unwrap_or_default().into()
+  str::from_utf8(&bytes).unwrap_or_default().into()
 }
 
-/// `tauri::AppHandle` extension for app utility dirs
+/// [tauri::AppHandle] extension for app utility dirs
 pub trait AppDirs {
   fn app_dir(&self) -> PathBuf;
 
