9cb55f4cc8ba1a968e86170b1199aa8bfa63b426
wladyslaw <vladislavvaliuchenkov@gmail.com>
2024-10-23 05:55:11 -0700

    small code improvements

--- apps/client/src-tauri/src/tools/bellingcat_tg.rs
+++ apps/client/src-tauri/src/tools/bellingcat_tg.rs
@@ -1,117 +1,130 @@
-// https://github.com/bellingcat/telegram-phone-number-checker
-// tags: phone numbers, telegram
+//! https://github.com/bellingcat/telegram-phone-number-checker
 
-use crate::utils::*;
-use serde_json::from_slice;
-use std::{collections::HashMap, fs, path::PathBuf, process::Stdio};
-use tauri::{
-  async_runtime::{block_on, spawn},
-  Emitter, Listener,
-};
-use tokio::{
-  io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
-  sync::Mutex,
-  time::{sleep, Duration},
-};
+mod utils {
+  use crate::utils::*;
+  use std::path::PathBuf;
 
-const APP_CMD: &str = "telegram-phone-number-checker";
+  pub const APP_CMD: &str = "telegram-phone-number-checker";
 
-fn get_cattg_dir(app: &App) -> PathBuf {
-  app.tools_dir().join(APP_CMD)
-}
-
-#[tauri::command(rename_all = "snake_case")]
-pub async fn cattg_install(app: App) -> CmdRes {
-  cmd("pipx")
-    .args(["install", APP_CMD])
-    .spawn()?
-    .wait()
-    .await?;
-
-  fs::create_dir_all(get_cattg_dir(&app))?;
-
-  Ok(())
-}
-
-#[tauri::command(rename_all = "snake_case")]
-pub async fn cattg_check() -> CmdRes {
-  let output = cmd(APP_CMD).arg("--help").output().await?;
-
-  let error_str = bytes_string(output.stderr);
-
-  if !error_str.is_empty() {
-    Err(AnyErr::msg(error_str))?;
+  pub fn get_cattg_dir(app: &App) -> PathBuf {
+    app.tools_dir().join(APP_CMD)
   }
 
-  Ok(())
+  schema!(CatPhone {
+    id?: u64,
+    username?: String,
+    usernames?: Vec<String>,
+    first_name?: String,
+    last_name?: String,
+    fake?: bool,
+    verified?: bool,
+    premium?: bool,
+    bot?: bool,
+    restricted?: bool,
+    restricted_reason?: String,
+    user_was_online?: String,
+    phone?: String,
+  });
 }
 
-schema!(CatPhone {
-  id: Option<u64>,
-  username: Option<String>,
-  usernames: Option<Vec<String>>,
-  first_name: Option<String>,
-  last_name: Option<String>,
-  fake: Option<bool>,
-  verified: Option<bool>,
-  premium: Option<bool>,
-  bot: Option<bool>,
-  restricted: Option<bool>,
-  restricted_reason: Option<String>,
-  user_was_online: Option<String>,
-  phone: Option<String>,
-});
+pub mod commands {
+  use super::utils::*;
+  use crate::utils::*;
 
-#[tauri::command(rename_all = "snake_case")]
-pub async fn cattg_phone(app: App, envs: [(&str, &str); 3], phone: &str) -> CmdRes<CatPhone> {
-  let cat_dir = get_cattg_dir(&app);
+  use serde_json::from_slice;
+  use std::{collections::HashMap, fs, process::Stdio};
+  use tauri::{
+    async_runtime::{block_on, spawn},
+    Emitter, Listener,
+  };
+  use tokio::{
+    io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
+    sync::Mutex,
+    time::{sleep, Duration},
+  };
 
-  let mut cmd = cmd(APP_CMD)
-    .envs(envs)
-    .args(["--phone-numbers", phone])
-    .current_dir(&cat_dir)
-    .stderr(Stdio::piped())
-    .stdin(Stdio::piped())
-    .spawn()?;
+  #[command(rename_all = "snake_case")]
+  pub async fn cattg_install(app: App) -> CmdRes {
+    cmd("pipx")
+      .args(["install", APP_CMD])
+      .spawn()?
+      .wait()
+      .await?;
 
-  let stderr = cmd.stderr.take().ctx("Failed to get stderr")?;
-  let stdin = Mutex::new(cmd.stdin.take().ctx("Failed to get stdin")?);
+    fs::create_dir_all(get_cattg_dir(&app))?;
 
-  let mut reader = BufReader::new(stderr).lines();
+    Ok(())
+  }
 
-  let otp_waiter = spawn(async move {
-    sleep(Duration::from_secs(4)).await;
+  #[command(rename_all = "snake_case")]
+  pub async fn cattg_check() -> CmdRes {
+    let output = cmd(APP_CMD).arg("--help").output().await?;
 
-    while let Ok(Some(line)) = reader.next_line().await {
-      if !line.contains("/TcpFull complete!") {
-        continue;
-      }
+    let error_str = bytes_string(output.stderr);
 
-      if app.emit("cat-otp-request", 0).is_err() {
+    if !error_str.is_empty() {
+      Err(AnyErr::msg(error_str))?;
+    }
+
+    Ok(())
+  }
+
+  #[command(rename_all = "snake_case")]
+  pub async fn cattg_phone(app: App, envs: [(&str, &str); 3], phone: &str) -> CmdRes<CatPhone> {
+    let cat_dir = get_cattg_dir(&app);
+
+    let mut cmd = cmd(APP_CMD)
+      .envs(envs)
+      .args(["--phone-numbers", phone])
+      .current_dir(&cat_dir)
+      .stderr(Stdio::piped())
+      .stdin(Stdio::piped())
+      .spawn()?;
+
+    let stderr = cmd.stderr.take().ctx("Failed to get stderr")?;
+    let stdin = Mutex::new(cmd.stdin.take().ctx("Failed to get stdin")?);
+
+    let otp_waiter = spawn(async move {
+      sleep(Duration::from_secs(4)).await;
+
+      let mut reader = BufReader::new(stderr).lines();
+
+      while let Ok(Some(line)) = reader.next_line().await {
+        if !line.contains("/TcpFull complete!") {
+          continue;
+        }
+
+        if app.emit("cat-otp-request", 0).is_err() {
+          return;
+        }
+
+        app.listen_any("cat-otp", move |event| {
+          block_on(async {
+            let mut stdin = stdin.lock().await;
+
+            let telegram_otp = event.payload();
+
+            if stdin
+              .write_all(format!("{telegram_otp}\n").as_bytes())
+              .await
+              .is_ok()
+            {
+              let _ = stdin.flush().await;
+            }
+          })
+        });
+
         return;
       }
+    });
 
-      app.listen_any("cat-otp", move |event| {
-        block_on(async {
-          let mut stdin = stdin.lock().await;
+    cmd.wait().await?;
+    otp_waiter.abort();
 
-          let telegram_otp = event.payload();
+    let res_file = fs::read(cat_dir.join("results.json"))?;
 
-          let _ = stdin.write(format!("{telegram_otp}\n").as_bytes()).await;
-          let _ = stdin.flush().await;
-        })
-      });
+    let mut res: HashMap<String, CatPhone> = from_slice(&res_file)?;
 
-      return;
-    }
-  });
-
-  cmd.wait().await?;
-  otp_waiter.abort();
-
-  let res_file = fs::read(cat_dir.join("results.json"))?;
-
-  let mut res: HashMap<String, CatPhone> = from_slice(&res_file)?;
-
-  Ok(res.remove(phone).ctx("Phone not found")?)
+    Ok(res.remove(phone).ctx("Phone not found")?)
+  }
 }
--- apps/client/src-tauri/src/tools/sherlock.rs
+++ apps/client/src-tauri/src/tools/sherlock.rs
@@ -1,39 +1,43 @@
-// https://github.com/sherlock-project/sherlock
-// nicknames
+//! https://github.com/sherlock-project/sherlock
 
-use crate::utils::*;
-
-#[tauri::command(rename_all = "snake_case")]
-pub async fn sherlock_install() -> CmdRes {
-  cmd("pipx")
-    .args(["install", "sherlock-project"])
-    .spawn()?
-    .wait()
-    .await?;
-
-  Ok(())
+mod utils {
+  pub const APP_CMD: &str = "sherlock";
 }
 
-const APP_CMD: &str = "sherlock";
+pub mod commands {
+  use super::utils::*;
+  use crate::utils::*;
 
-#[tauri::command(rename_all = "snake_case")]
-pub async fn sherlock_check() -> CmdRes<bool> {
-  let output = cmd(APP_CMD).arg("--version").output().await?;
+  #[command(rename_all = "snake_case")]
+  pub async fn sherlock_install() -> CmdRes {
+    cmd("pipx")
+      .args(["install", "sherlock-project"])
+      .spawn()?
+      .wait()
+      .await?;
 
-  let error_str = bytes_string(output.stderr);
-
-  if !error_str.is_empty() {
-    Err(AnyErr::msg(error_str))?;
+    Ok(())
   }
 
-  Ok(true)
-}
+  #[command(rename_all = "snake_case")]
+  pub async fn sherlock_check() -> CmdRes<bool> {
+    let output = cmd(APP_CMD).arg("--version").output().await?;
 
-#[tauri::command(rename_all = "snake_case")]
-pub fn sherlock_search(nickname: &str) -> CmdRes {
-  cmd(APP_CMD)
-    .args([nickname, "--nsfw", "--browse"])
-    .spawn()?;
+    let error_str = bytes_string(output.stderr);
 
-  Ok(())
+    if !error_str.is_empty() {
+      Err(AnyErr::msg(error_str))?;
+    }
+
+    Ok(true)
+  }
+
+  #[command(rename_all = "snake_case")]
+  pub fn sherlock_search(nickname: &str) -> CmdRes {
+    cmd(APP_CMD)
+      .args([nickname, "--nsfw", "--browse"])
+      .spawn()?;
+
+    Ok(())
+  }
 }
