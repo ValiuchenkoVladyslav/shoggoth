3453eb6027bf6f93f4bdac720699d1608eb3ecaa
wladyslaw <vladislavvaliuchenkov@gmail.com>
2024-10-25 13:07:25 -0700

    added nickname selection

--- apps/extension/src/service_worker.ts
+++ apps/extension/src/service_worker.ts
@@ -1,17 +1,23 @@
-import type { Text, Url } from "~/gen/core";
+import type { Nickname, Text, Url } from "~/gen/core";
 import { post } from "./lib";
 
 const addUrlId = "shoggoth:add-url";
 const addTextId = "shoggoth:add-text";
+const addNicknameId = "shoggoth:add-nickname";
 
 browser.contextMenus.create({
   id: addUrlId,
-  title: "Send link to Shoggoth",
+  title: "Send Link to Shoggoth",
   contexts: ["link"],
 });
 browser.contextMenus.create({
   id: addTextId,
-  title: "Send text to Shoggoth",
+  title: "Send Text to Shoggoth",
+  contexts: ["selection"],
+});
+browser.contextMenus.create({
+  id: addNicknameId,
+  title: "Send Nickname to Shoggoth",
   contexts: ["selection"],
 });
 
@@ -20,7 +26,13 @@ browser.contextMenus.onClicked.addListener((info) => {
     post<Url>("add-url", { url: info.linkUrl });
   }
 
-  if (info.menuItemId === addTextId && info.selectionText) {
-    post<Text>("add-text", { text: info.selectionText });
+  if (info.selectionText) {
+    if (info.menuItemId === addTextId) {
+      post<Text>("add-text", { text: info.selectionText });
+    }
+
+    if (info.menuItemId === addNicknameId) {
+      post<Nickname>("add-nickname", { nickname: info.selectionText });
+    }
   }
 });
