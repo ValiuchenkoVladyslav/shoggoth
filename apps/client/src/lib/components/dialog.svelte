37fb13466f6a87f6eaca5d03adb0c9e53600e238
wladyslaw <vladislavvaliuchenkov@gmail.com>
2024-10-30 10:35:46 -0700

    removed redundant class

--- apps/client/src/lib/components/dialog.svelte
+++ apps/client/src/lib/components/dialog.svelte
@@ -54,6 +54,6 @@
 
 <!-- svelte-ignore a11y_click_events_have_key_events -->
 <!-- svelte-ignore a11y_no_static_element_interactions -->
-<div onclick={() => dialogRef.showModal()} class="*:w-full">
+<div onclick={() => dialogRef.showModal()}>
   {@render trigger()}
 </div>
