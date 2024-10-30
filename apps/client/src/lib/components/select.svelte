55de30b4218d7c9f026988c5b8251662da018439
wladyslaw <vladislavvaliuchenkov@gmail.com>
2024-10-30 10:34:43 -0700

    moved to details element

--- apps/client/src/lib/components/select.svelte
+++ apps/client/src/lib/components/select.svelte
@@ -1,67 +1,66 @@
 <script lang="ts">
-  import type { Snippet } from "svelte";
   import { ChevronDown, ChevronUp } from "lucide-svelte";
+  import type { Snippet } from "svelte";
 
-  type SelectProps = {
-    initialText: string;
+  type Props = {
+    initText: string;
     value?: string;
     children: Snippet;
   };
 
-  let { initialText, children, value = $bindable() }: SelectProps = $props();
+  let { initText, children, value = $bindable() }: Props = $props();
+
+  let detailsRef: HTMLDetailsElement;
+  let summaryRef = $state<HTMLElement>();
+  // save original width of details
+  let width = $derived(summaryRef?.getBoundingClientRect().width);
 
   let open = $state(false);
 
   let textValue = $state<string>();
-
-  let btnRef = $state<HTMLButtonElement>();
-  // to save original width of button
-  let width = $derived(btnRef?.getBoundingClientRect().width);
 </script>
 
-<svelte:window
-  onclick={(e) => (open && !btnRef?.contains(e.target as Node)) && (open = false)}
-  oncontextmenu={(e) => (open && !btnRef?.contains(e.target as Node)) && (open = false)}
-  onkeydown={(e) => e.key === "Escape" && (open = false)}
-/>
-
-<div>
-  <button
-    bind:this={btnRef}
+<details bind:this={detailsRef} bind:open={open}>
+  <summary
+    bind:this={summaryRef}
     class={`
-      light min-w-36 rounded-lg min-h-10 flex items-center justify-between px-3 gap-3 font-semibold
+      cursor-pointer select-none light rounded-lg h-full flex items-center justify-between px-3 gap-3 font-semibold
       ${open ? "rounded-b-none" : ""}
     `}
     style={width ? `width: ${width}px` : ""}
-    onclick={() => open = !open}
   >
-    {textValue || initialText}
+    {textValue || initText}
 
     {#if open}
       <ChevronUp size={20} />
     {:else}
       <ChevronDown size={20} />
     {/if}
-  </button>
+  </summary>
 
-  {#if open}
-    <!-- svelte-ignore a11y_click_events_have_key_events -->
-    <!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
-    <menu
-      onclick={(evt) => {
-        open = false;
+  <!-- svelte-ignore a11y_click_events_have_key_events -->
+  <!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
+  <menu
+    onclick={(evt) => {
+      const option = (evt.target as Element).closest("option");
+      if (!option) return;
 
-        value = (evt.target as Element).closest("option")?.value;
-        textValue = (evt.target as Element).closest("option")?.textContent ?? undefined;
-      }}
-      class={`
-        light rounded-b-lg absolute
-        outline outline-2 outline-black outline-t-0
-        *:p-2 hover:*:bg-black/10 hover:*:cursor-pointer *:font-semibold
-      `}
-      style={`width: ${width}px`}
-    >
-      {@render children()}
-    </menu>
-  {/if}
-</div>
+      open = false;
+      value = option.value;
+      textValue = option.textContent ?? value;
+    }}
+    class={`
+      light rounded-b-lg fixed
+      outline outline-2 outline-black outline-t-0
+      *:p-2 hover:*:bg-black/10 hover:*:cursor-pointer *:font-semibold
+    `}
+    style={width ? `width: ${width}px` : ""}
+  >
+    {@render children()}
+  </menu>
+</details>
+
+<svelte:window
+  onmousedown={(e) => detailsRef?.contains(e.target as Node) || (open = false)}
+  onkeydown={(e) => e.key === "Escape" && (open = false)}
+/>
