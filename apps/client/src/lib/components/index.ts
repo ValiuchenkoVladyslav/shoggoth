08e0e8ede59d1c6243239184e62a8ed7fd46198b
wladyslaw <vladislavvaliuchenkov@gmail.com>
2024-10-30 16:03:20 -0700

    added textarea

--- apps/client/src/lib/components/index.ts
+++ apps/client/src/lib/components/index.ts
@@ -4,5 +4,6 @@ import Dialog from "./dialog.svelte";
 import Layout from "./layout.svelte";
 import NavLink from "./nav-link.svelte";
 import Select from "./select.svelte";
+import Textarea from "./textarea.svelte";
 
-export { Btn, NavLink, Layout, CtxMenu, Dialog, Select };
+export { Btn, NavLink, Layout, CtxMenu, Dialog, Select, Textarea };
---
+++ apps/client/src/lib/components/textarea.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { cn } from '$lib/utils';
+  import type { HTMLTextareaAttributes } from 'svelte/elements';
+
+  let { children, class: _class, ...props }: HTMLTextareaAttributes = $props();
+</script>
+
+<textarea
+  class={cn("dark nowheel nodrag border border-2 border-gray-800 rounded-md py-1 px-2 min-h-[3lh]", _class)}
+  {...props}
+></textarea>
