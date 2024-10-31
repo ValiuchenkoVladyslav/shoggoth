9c2dd44c7349519a5535ffb661da42e2099e9b17
wladyslaw <vladislavvaliuchenkov@gmail.com>
2024-10-31 09:21:06 -0700

    added bindable value

--- apps/client/src/lib/components/textarea.svelte
+++ apps/client/src/lib/components/textarea.svelte
@@ -2,10 +2,11 @@
   import { cn } from '$lib/utils';
   import type { HTMLTextareaAttributes } from 'svelte/elements';
 
-  let { children, class: _class, ...props }: HTMLTextareaAttributes = $props();
+  let { class: _class, value = $bindable(), ...props }: HTMLTextareaAttributes = $props();
 </script>
 
 <textarea
+  bind:value={value}
   class={cn("dark nowheel nodrag border border-2 border-gray-800 rounded-md py-1 px-2 min-h-[3lh]", _class)}
   {...props}
 ></textarea>
