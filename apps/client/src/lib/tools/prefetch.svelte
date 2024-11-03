902e071b729c11fd35e0ebfbf247038d326b5408
wladyslaw <vladislavvaliuchenkov@gmail.com>
2024-11-03 06:27:41 -0800

    added bellingcat & infoga prefetching

--- apps/client/src/lib/tools/prefetch.svelte
+++ apps/client/src/lib/tools/prefetch.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
   import { browser } from "$app/environment";
   import { get } from "svelte/store";
-  import { sherlockStatusQuery } from "./sherlock/tauri-api";
+  import { Sherlock } from "./sherlock/tauri-api";
+  import { Infoga } from "./phone-infoga/tauri-api";
+  import { BellingCatTg } from "./bellingcat-tg/tauri-api";
 
   if (browser) {
-    get(sherlockStatusQuery());
+    get(Sherlock.statusQuery());
+    get(Infoga.statusQuery());
+    get(BellingCatTg.statusQuery());
   }
 </script>
